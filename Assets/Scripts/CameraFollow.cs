f530b9e0e21e69ca14d273468699fdeafd7eb3a2
Boomcrasc078 <mose.g07@gmail.com>
2026-09-21 16:16:33 +0200

    Added camera follow and zoom

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,24 +1,42 @@
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform[] cameraTargets;
     [SerializeField] private Vector3 cameraOffset = new Vector3(0, 0, -10);
-    [SerializeField] private float cameraSmoothing;
+    [SerializeField] private float cameraSmoothing = 5f;
+    [SerializeField] private float zoomPadding = 2f;
+    [SerializeField] private float minZoom = 5f;
+    private Transform[] cameraTargets;
 
     void LateUpdate()
     {
         cameraTargets = GameObject.FindGameObjectsWithTag("Player").Select(player => player.transform).ToArray();
-        Vector2 middlePosition = Vector2.zero;
+        if (cameraTargets.Length == 0)
+        {
+            return;
+        }
+
+        Bounds playerBounds = new Bounds(cameraTargets[0].position, Vector3.zero);
         foreach (var cameraTarget in cameraTargets)
         {
-            middlePosition = new Vector2(middlePosition.x + cameraTarget.position.x, middlePosition.y + cameraTarget.position.y);
+            playerBounds.Encapsulate(cameraTarget.position);
         }
-        middlePosition = new Vector2(middlePosition.x / cameraTargets.Length, middlePosition.y / cameraTargets.Length);
 
-        Vector3 newPosition = Vector3.Lerp(transform.position, (Vector3)middlePosition + cameraOffset, cameraSmoothing * Time.deltaTime);
-        transform.position = newPosition;
+        Camera camera = GetComponent<Camera>();
+        float requiredHalfHeight = Mathf.Max(playerBounds.size.x / camera.aspect, playerBounds.size.y) * 0.5f + zoomPadding;
+        Vector3 desiredPosition = playerBounds.center + cameraOffset;
+
+        if (camera.orthographic)
+        {
+            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, Mathf.Max(minZoom, requiredHalfHeight), cameraSmoothing * Time.deltaTime);
+        }
+        else
+        {
+            float requiredDistance = requiredHalfHeight / Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            desiredPosition.z = playerBounds.center.z - Mathf.Max(Mathf.Abs(cameraOffset.z), requiredDistance);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, cameraSmoothing * Time.deltaTime);
     }
 }

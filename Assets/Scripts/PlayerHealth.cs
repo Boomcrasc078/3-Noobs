bdc42e48f87d2b9e12a3d5bbf28a50eb4753e005
Boomcrasc078 <mose.g07@gmail.com>
2026-09-21 15:28:06 +0200

    Added old project

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,71 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] private int startingHealth = 5;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Image image;
+    [SerializeField] private Color normalHealthColor;
+    [SerializeField] private Color lowHealthColor;
+    private int currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = startingHealth;
+        UpdateHealthbar();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHealthbar();
+        Debug.Log(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        currentHealth = startingHealth;
+        UpdateHealthbar();
+        transform.position = spawnPosition.position;
+        GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+    }
+
+    private void UpdateHealthbar()
+    {
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 2)
+        {
+            image.color = lowHealthColor;
+        }
+        else
+        {
+            image.color = normalHealthColor;
+        }
+    }
+
+    public bool RestoreHealth(int healthToRestore)
+    {
+        if (currentHealth >= startingHealth)
+        {
+            return false;
+        }
+        currentHealth += healthToRestore;
+        UpdateHealthbar();
+
+if(currentHealth > startingHealth)
+        {
+            currentHealth = startingHealth;
+        }
+
+        return true;
+    }
+}
